b32d5950a0ed35e1bdda9e4160615169cc1ede67
Shojibur Rahman <41312353+Shojiburman@users.noreply.github.com>
2023-03-17 16:41:06 +0600

    Add files via upload

---
+++ form manupulation.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+<script type="text/javascript"> 
+	document.querySelector("[name='name']").value = 'shojib';
+	document.querySelector("[value='male']").checked = true;
+	document.querySelector("[value='cricket']").checked = true;
+	document.querySelector("textarea").value = 'aba bab cba';
+	document.querySelector("[value='sign-up']").click();
+</script>
+</html>
+
